124be78064f639753147a19357eba3f84d4c3b82
akash agarwal <akash.ka01@gmail.com>
2025-03-08 14:05:04 +0530

    remove transform

--- src/api/controllers/teacher.controller.ts
+++ src/api/controllers/teacher.controller.ts
@@ -301,8 +301,6 @@ exports.listTeachers = async (
       })
     );
 
-    console.log('teachers', JSON.stringify(teachers));
-
     teachers = teachers.filter((teacher: { isActive: Boolean }) => teacher.isActive === true);
 
     // Step 2: Filter based on searchQuery
@@ -376,12 +374,6 @@ exports.listTeachers = async (
     const pageSize = 10;
     const startIndex = (page - 1) * pageSize;
     const paginatedTeachers = teachers.slice(startIndex, startIndex + pageSize);
-
-    console.log('paginatedTeachers', JSON.stringify(paginatedTeachers));
-
-    // Step 6: Transform and return results
-    const transformedTeachers = paginatedTeachers.map((teacher: any) => teacher?.transform());
-    console.log('transformedTeachers', JSON.stringify(transformedTeachers));
 
     res.json(paginatedTeachers);
   } catch (error) {
