96b061748dead50a9f8090309b4f11d3caff3270
akash agarwal <akash.ka01@gmail.com>
2025-03-12 11:52:46 +0530

    route

--- src/api/routes/v1/resources.route.ts
+++ src/api/routes/v1/resources.route.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 const controller = require('../../controllers/resources.controller');
 
 router.route('/generateEducationalContent').post(controller.generateEducationalContent);
-router.get('/').post(controller.listResources);
+router.get('/').get(controller.listResources);
 
 module.exports = router;
