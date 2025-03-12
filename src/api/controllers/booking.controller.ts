11f59be84df25d1a76511396ce4345f00526f52d
akash agarwal <akash.ka01@gmail.com>
2025-03-12 16:59:01 +0530

    dummy data

--- src/api/controllers/booking.controller.ts
+++ src/api/controllers/booking.controller.ts
@@ -220,37 +220,30 @@ exports.getBookingByFilters = async (req: Request, res: Response) => {
     const { teacherId, studentId, parentId, batchId, status, subjects, boards, classes, isActive } = req.query;
 
     let bookings = await Booking.find();
-    console.log('bookings 1', JSON.stringify(bookings));
 
     if (teacherId) {
       bookings = bookings.filter((b: any) => b.teacherId.toString() === teacherId);
     }
-    console.log('bookings 2', JSON.stringify(bookings));
 
     if (studentId) {
       bookings = bookings.filter((b: any) => b.studentId.toString() === studentId);
     }
-    console.log('bookings 3', JSON.stringify(bookings));
 
     if (parentId) {
       bookings = bookings.filter((b: any) => b.parentId.toString() === parentId);
     }
-    console.log('bookings 4', JSON.stringify(bookings));
 
     if (isActive) {
       bookings = bookings.filter((b: any) => b.isActive.toString() === isActive.toString());
     }
-    console.log('bookings 5', JSON.stringify(bookings));
 
     if (batchId) {
       bookings = bookings.filter((b: any) => b.batchId === batchId);
     }
-    console.log('bookings 6', JSON.stringify(bookings));
 
     if (status) {
       bookings = bookings.filter((b: any) => b.status === status);
     }
-    console.log('bookings 7', JSON.stringify(bookings));
 
     bookings.map(async (b: any) => {
       b.teacher = await Teacher.findById(b.teacherId);
@@ -258,7 +251,6 @@ exports.getBookingByFilters = async (req: Request, res: Response) => {
       b.parent = await Parent.findOne({ userId: b.parentId });
       b.batch = await ClassBatch.findById(b.batchId);
     });
-    console.log('bookings 8', JSON.stringify(bookings));
 
     res.status(200).json({
       success: true,
