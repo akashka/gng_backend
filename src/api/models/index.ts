ff9e90f14bcbf4fc9166f808eb81439985c19653
akash-reckonsys <akash@reckonsys.com>
2025-06-29 12:02:08 +0530

    rate

--- src/api/models/index.ts
+++ src/api/models/index.ts
@@ -3,4 +3,4 @@ export const RefreshToken = require('./refreshToken.model');
 export const StaticData = require('./staticData.model');
 export const Location = require('./location.model');
 export const Employee = require('./employee.model');
-export const Rate = require('./rates.model');
+export const Rate = require('./rate.model');
